19d3d0248f6cd6a6f59b1b21170773cd30b7955c
Mutyam3 <bhargavreddymutyam@gmail.com>
2024-11-30 10:17:41 +0530

    last commit

--- SessionIntro/server.js
+++ SessionIntro/server.js
@@ -2,19 +2,57 @@ var express = require('express')
 var app = express()
 
 var expressSession = require('express-session')
+var bodyParser = require('body-parser')
+app.use(express.static(__dirname + '/public'))
+
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(expressSession({
     secret: 'mass raa ',
-    cookie: {maxAge: 60000}
+    cookie: {maxAge: 24*60*60*1000} // 6000 -> 6seconds | 60000 -> 1 minute | 10minutes -> 10*60*1000 | 1day ==> 24hr ==> 24*60*60*1000
 }))
 
+app.post('/login',(req, res)=>{
 
-app.get('/', (req, res)=>{
-    res.send('hi session')
+    req.session.username = req.body.username
+    res.redirect('/')
 })
 
+app.use((req, res, next)=>{
 
+    // console.log(req.session)
+    // console.log(req.sessionID)
+    // if(req.session.count){
+    //     req.session.count++
+    // }
+    // else 
+    // {
+    //     req.session.count = 1
+    // }
+    // console.log(req.sessionID + " : " + req.session.count)
 
+    if(req.session.username)
+    {
+        next()
+    }
+    else 
+    {
+        res.redirect('/login.html')
+    }
+    
+})
+
+app.get('/', (req, res)=>{
+    res.send('hi sessions')
+})
+
+app.get('/abc', (req, res)=>{
+    res.send('abc request raa idi')
+})
+
+app.get('/mutyam', (req, res)=>{
+    res.send('mutyam request vachai')
+})
 
 
 
